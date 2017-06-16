f9d85d487d07f01e6406bdc699eeb5448faba74d
Michael Bartido <mbartido@ucsc.edu>
2017-06-16 16:29:41 -0700

    Made custom title bar

--- PHUL/app/src/main/java/com/example/jamberjong/phul/MainActivity.java
+++ PHUL/app/src/main/java/com/example/jamberjong/phul/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.jamberjong.phul;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.support.v7.app.ActionBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Gets the action bar
+        ActionBar ab = getSupportActionBar();
+        // Sets the background of the action bar
+        ab.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+        // Makes a new text view
+        TextView tv = new TextView(getApplicationContext());
+        // Creates layout params for textView
+        ViewGroup.LayoutParams lp = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        // Apply the Layout Params to textView
+        tv.setLayoutParams(lp);
+        // Sets the text for the bar
+        tv.setText("PHUL");
+        // Sets font color
+        tv.setTextColor(Color.parseColor("#FFFFFF"));
+        // Centers text
+        tv.setGravity(Gravity.CENTER);
+        // Sets a custom font for title
+        Typeface custom = Typeface.createFromAsset(getAssets(), "fonts/DroidSans-Bold.ttf");
+        tv.setTypeface(custom);
+        // Changes the text size
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+        // Sets ActionBar display option
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        // Set newly created TextView as ActionBar custom view
+        ab.setCustomView(tv);
     }
 }
